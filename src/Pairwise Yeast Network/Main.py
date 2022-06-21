3027b8293615c73ce624650a9e17c7a5373586b2
Cole McGuire <cmcguir1@trinity.edu>
2022-06-21 09:27:31 -0500

    Just switched the name of my local repos

--- src/Pairwise Yeast Network/Main.py
+++ src/Pairwise Yeast Network/Main.py
@@ -9,25 +9,6 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 def main():
-
-
-    # for i in range(2):
-    #     printModelData = PairwiseYeastData('Yeast Resources/Datasets/Primig and Brem',4,subset=10000,recur=False)
-    # print(f'Time: {(time.time()-start)/60}')
-    # start = time.time()
-    # for i in range(2):
-    #     printModelData = PairwiseYeastData('Yeast Resources/Datasets/Primig and Brem',4,subset=100000,recur=False)
-    # print(f'Time: {(time.time()-start)/60}')
-    # start = time.time()
-    # printModelData = PairwiseYeastData('Yeast Resources/Datasets/Primig and Brem',4,subset=1000000,recur=False)
-    # print(f'Time: {(time.time()-start)/60}')
-    # start = time.time()
-    # printModelData = PairwiseYeastData('Yeast Resources/Datasets/Primig and Brem',4,subset=11179356,recur=False)
-    # print(f'Time: {(time.time()-start)/60}')
-
-    # primAndBrem = PairwiseYeastData('./Yeast Resources/Datasets/Primig and Brem',4,subset=10,recur=False,sort=False,filterMissingGenes=True)
-    # primAndBrem.calculateCorrelations()
-
     #This comment should appear on github now
 
     # folds = GeneFolds(numFolds=4)
@@ -42,7 +23,6 @@ def main():
     print(f'Time to load datasets: {(time.time()-start)/60} minutes')
 
     regularTest = []
-    # noRegularTest = []
     for i in range(4):
         regularTest.append(PairwiseModel(modelData,i,'20x1','Spell/Test','Regular430'))
         # noRegularTest.append(PairwiseModel(modelData,i,'20x10x1','Spell/Test','NoRegular50'))
@@ -54,22 +34,9 @@ def main():
     #     model.testNetworkValidation(regularize=False,limitNegative=True)
     
 
-    # syntheticData = PairwiseYeastData('./Yeast Resources/Datasets/Synthetic/Medium 36-264',4,subset=10000,numDatasets=5,recur=False,sort=False,filterMissingGenes=True)
-    # syntheticModel = PairwiseModel(syntheticData,0,'1','Pairwise/Synthetic/Test','MedTest')
-    # syntheticModel.trainNetwork(1000,printLoss=True,printTensors=True)
-    # syntheticModel.testNetworkValidation()
-
-    # smallData = PairwiseYeastData('./Yeast Resources/Datasets/Primig and Brem',4,subset=100000,numDatasets=3,recur=False,sort=False,filterMissingGenes=True)
-    # smallModel = PairwiseModel(smallData,0,'10x10x1','Pairwise/Primig and Brem Test','TestResult')
-    # smallModel.trainNetwork(10000,printLoss=True,printTensors=True)
-    # smallModel.testNetworkValidation()
 
 
 
-    # printModel = PairwiseModel(printModelData,0,'20x1','Spell/Test','TestRun')
-    # printModel.trainNetwork(10000,printLoss=True)
-
-    # GalitskiData = PairwiseYeastData('Yeast Resources/Datasets/All Spell/all spell datasets/Wyrick_1999_PMID_10586882',4,subset=100000,numDatasets=1,recur=False)
 
 
 
