41a44dc1e8cd61a47de2d1ac59749cae35470677
2021-08-24 <cmcguir1@dias02.cs.trinity.edu>
2022-06-28 09:18:51 -0500

    Collecting testing outputs

--- src/PairwiseYeastNetwork/Parallel/Model2.py
+++ src/PairwiseYeastNetwork/Parallel/Model2.py
@@ -8,7 +8,7 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 def main():
     for i in range(4):
-        test(i,'Meiosis',posGenes='./Yeast Resources/GeneSets/GO0007127_Pos.txt',negGenes='./Yeast Resources\GeneSets\GO0007127_Neg.txt',foldFile='./Yeast Resources/GeneSets/GO0007127Folds1.csv',folderName='Spell/OtherGOTerms')
+        test(i,'Meiosis',posGenes='./Yeast Resources/GeneSets/GO0007127_Pos.txt',negGenes='./Yeast Resources/GeneSets/GO0007127_Neg.txt',foldFile='./Yeast Resources/GeneSets/GO0007127Folds1.csv',folderName='Spell/OtherGOTerms')
     # #test(0,'ModelTest2',batch_size=20,epoch=1000,datasets=10)
     # test(0,'TestMeiosis',posGenes='./Yeast Resources/GeneSets/GO0007127_Pos.txt',negGenes='./Yeast Resources\GeneSets\GO0007127_Neg.txt',foldFile='./Yeast Resources/GeneSets/GO0007127Folds1.csv',folderName='Spell/OtherGOTerms',epoch=100,datasets=10)
 
