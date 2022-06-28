91247fc71d101f982d300b58fdaba3ca79f7af09
Cole McGuire <cmcguir1@trinity.edu>
2022-06-28 16:07:01 -0500

    Structure Tests

--- src/PairwiseYeastNetwork/Parallel/Model5.py
+++ src/PairwiseYeastNetwork/Parallel/Model5.py
@@ -8,8 +8,8 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 def main():
     for i in range(4):
-        # test(i,f'StructureTest',folderName='Spell/Structure',foldFile=f'./Yeast Resources/Datasets/All Spell/GeneFolds3.csv',structure=sys.argv[1])
-        test(0,f'TestStructure',folderName='Spell/Structure',foldFile=f'./Yeast Resources/Datasets/All Spell/GeneFolds3.csv',structure=sys.argv[1],epoch=100,datasets=5)
+        test(i,f'StructureTest',folderName='Spell/Structure',foldFile=f'./Yeast Resources/Datasets/All Spell/GeneFolds3.csv',structure=sys.argv[1])
+        # test(0,f'TestStructure',folderName='Spell/Structure',foldFile=f'./Yeast Resources/Datasets/All Spell/GeneFolds3.csv',structure=sys.argv[1],epoch=100,datasets=5)
 
 
 if __name__ == '__main__':
