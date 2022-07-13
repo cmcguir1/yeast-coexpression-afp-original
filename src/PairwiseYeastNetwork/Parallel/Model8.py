7ccf6e051a576500d0d5a04b3e4d932b1648d801
Cole McGuire <cmcguir1@trinity.edu>
2022-07-13 15:35:17 -0500

    Graph Testing fix

--- src/PairwiseYeastNetwork/Parallel/Model8.py
+++ src/PairwiseYeastNetwork/Parallel/Model8.py
@@ -14,7 +14,7 @@ ssl._create_default_https_context = ssl._create_unverified_context
 def main():
     
     modelData = PairwiseYeastData(f'Yeast Resources/Datasets/All Spell/original',4,subset=200000,numDatasets=113,statsDictLoc='./Yeast Resources/Datasets/All Spell/revisedStatsDict.csv',recalc=False,foldFile='./Yeast Resources/Datasets/All Spell/GeneFolds1.csv',posGenes='./Yeast Resources/positives_00_go04-15-07.txt',negGenes='./Yeast Resources/negatives_00_go04-15-07.txt',recur=False,sort=False)
-    graph = YeastGraph('./Yeast Resources/Pairwise/Spell/Original/Original_100x1_Net_fold1.pth',modelData,'113x200x1',posFile='./Yeast Resources\positives_00_go04-15-07.txt',negFile='./Yeast Resources/negatives_00_go04-15-07.txt',agnFile='./Yeast Resources/agnostic_01_underannotated.txt')
+    graph = YeastGraph('./Yeast Resources/Pairwise/Spell/Original/Original_100x1_Net_fold1.pth',modelData,'113x100x1',posFile='./Yeast Resources\positives_00_go04-15-07.txt',negFile='./Yeast Resources/negatives_00_go04-15-07.txt',agnFile='./Yeast Resources/agnostic_01_underannotated.txt')
     graph.feedForward('./Yeast Resources/GraphResults/OriginalPairScores.csv')
     graph.rankGenes('./Yeast Resources/GraphResults/OriginalRankGenes.csv')
 
