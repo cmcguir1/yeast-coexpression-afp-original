1edb2be23e74f6f6b3c6e168a1c2f065114922aa
Cole McGuire <cmcguir1@trinity.edu>
2022-07-16 18:17:26 -0500

    Making original yeast graphs

--- src/PairwiseYeastNetwork/Parallel/Model12.py
+++ src/PairwiseYeastNetwork/Parallel/Model12.py
@@ -13,13 +13,13 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 def main():
     modelData = PairwiseYeastData(f'Yeast Resources/Datasets/All Spell/original',4,subset=200000,numDatasets=113,statsDictLoc='./Yeast Resources/Datasets/All Spell/revisedStatsDict.csv',recalc=False,foldFile='./Yeast Resources/Datasets/All Spell/GeneFolds3.csv',posGenes='./Yeast Resources/positives_00_go04-15-07.txt',negGenes='./Yeast Resources/negatives_00_go04-15-07.txt',recur=False,sort=False)
-    model = PairwiseModel(modelData,int(sys.argv[1]),f'{sys.argv[2]}x1','Spell/OriginalRerun',f'Original')
-    model.trainNetwork(10000,printLoss=True)
-    model.testNetworkTraining(limitNegative=True)
-    model.testNetworkValidation(limitNegative=True)
+    # model = PairwiseModel(modelData,int(sys.argv[1]),f'{sys.argv[2]}x1','Spell/OriginalRerun',f'Original')
+    # model.trainNetwork(10000,printLoss=True)
+    # model.testNetworkTraining(limitNegative=True)
+    # model.testNetworkValidation(limitNegative=True)
 
-    graph = YeastGraph(f'./Yeast Resources/Pairwise/Spell/OriginalRerun/Original_{sys.argv[2]}x1_Net_fold',modelData,f'113x{sys.argv[1]}x1',folder=f'Original_113x{sys.argv[2]}x1',posFile='./Yeast Resources/positives_00_go04-15-07.txt',negFile='./Yeast Resources/negatives_00_go04-15-07.txt',agnFile='./Yeast Resources/agnostic_01_underannotated.txt',includeAll=False)
-    graph.feedForward(f'Original_113x{sys.argv[2]}x1_Pairs.csv')
+    graph = YeastGraph(f'./Yeast Resources/Pairwise/Spell/OriginalRerun/Original_{sys.argv[2]}x1_Net_fold',modelData,f'113x{sys.argv[2]}x1',folder=f'Original_113x{sys.argv[2]}x1',posFile='./Yeast Resources/positives_00_go04-15-07.txt',negFile='./Yeast Resources/negatives_00_go04-15-07.txt',agnFile='./Yeast Resources/agnostic_01_underannotated.txt',includeAll=False)
+    graph.feedForward(f'Original_113x{sys.argv[2]}x1_Pairs.csv',fold=int(sys.argv[1]))
     graph.rankGenes(f'Original_113x{sys.argv[2]}x1_Ranked.csv')
 
 
