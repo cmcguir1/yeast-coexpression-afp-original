3139f97500e9cffd2fa04d5583c36a5378abda2c
Cole McGuire <cmcguir1@trinity.edu>
2022-07-21 01:45:47 -0500

    Making Modern Pairs Graph

--- src/PairwiseYeastNetwork/Parallel/Model2.py
+++ src/PairwiseYeastNetwork/Parallel/Model2.py
@@ -19,9 +19,9 @@ def main():
     # model.testNetworkValidation(limitNegative=True)
 
     graph = YeastGraph(f'./Yeast Resources/Pairwise/Spell/430Standard/Regular_{sys.argv[2]}x1_Net_fold',modelData,f'430x{sys.argv[2]}x1',folder=f'Standard_430x{sys.argv[2]}x1',posFile='./Yeast Resources/positives_00_go04-15-07.txt',negFile='./Yeast Resources/negatives_00_go04-15-07.txt',agnFile='./Yeast Resources/agnostic_01_underannotated.txt',includeAll=False)
-    graph.feedForward(f'Standard_430x{sys.argv[2]}x1_Pairs.csv',fold=int(sys.argv[1]))
-    # graph.recombineFolds(f'PosPairsFold','AgnPairsFold',f'Standard_430x{sys.argv[2]}x1_Pairs.csv')
-    # graph.rankGenes(f'Standard_430x{sys.argv[2]}x1_Ranked.csv')
+    #graph.feedForward(f'Standard_430x{sys.argv[2]}x1_Pairs.csv',fold=int(sys.argv[1]))
+    graph.recombineFolds(f'PosPairsFold','AgnPairsFold',f'Standard_430x{sys.argv[2]}x1_Pairs.csv')
+    graph.rankGenes(f'Standard_430x{sys.argv[2]}x1_Ranked.csv')
     # for i in range(4):
     #     graph.filterAgn(f'./Yeast Resources/GraphResults/Original_Rerun_113x20x1/PosPairsFold{i+1}.csv')
 
