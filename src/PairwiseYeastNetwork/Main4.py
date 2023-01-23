a3a2ce05ffecd601e1bb89289d7544895a041440
cmcguir1 <cmcguir1@trinity.edu>
2023-01-22 22:13:25 -0600

    Pushing results

--- src/PairwiseYeastNetwork/Main4.py
+++ src/PairwiseYeastNetwork/Main4.py
@@ -20,8 +20,8 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 def main():
     
-    #graph = AllGoGraph('./Yeast Resources/Pairwise/Spell/AllGO_Original/Original_113x200x92_Net_fold','113x200x92','AllGO_Original',memMapLoc='../YeastMemMap/YeastCorrDictionary.dat')
-    graph = AllGoGraph('./Yeast Resources/Pairwise/Spell/AllGO_Original/Original_113x200x92_Net_fold','113x200x92','AllGO_Original',ontologyDataset='original')
+    graph = AllGoGraph('./Yeast Resources/Pairwise/Spell/AllGO_Original/Original_113x200x92_Net_fold','113x200x92','AllGO_Original',memMapLoc='../YeastMemMap/YeastCorrDictionary.dat',ontologyDataset='original')
+    #graph = AllGoGraph('./Yeast Resources/Pairwise/Spell/AllGO_Original/Original_113x200x92_Net_fold','113x200x92','AllGO_Original',ontologyDataset='original')
     graph.feedForward(int(sys.argv[1]))
     #graph.rankGenes()
 
