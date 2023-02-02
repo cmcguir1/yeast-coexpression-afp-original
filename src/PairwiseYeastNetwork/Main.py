0529d5c46657eebacb091e2ce8f93622dcf159af
Cole McGuire <cmcguir1@trinity.edu>
2023-02-01 21:02:30 -0600

    Now overfit

--- src/PairwiseYeastNetwork/Main.py
+++ src/PairwiseYeastNetwork/Main.py
@@ -20,7 +20,7 @@ ssl._create_default_https_context = ssl._create_unverified_context
 def main():
 
     #GOTest = AllGoModel(i,sys.argv[1],'AllGO_Original_Parameter','Original',foldFile='./src/PairwiseYeastNetwork/AllGOGeneFold_Orignial_1.csv',ontologyDataset='original',inputDropout=float(sys.argv[2]),hiddenDropout=float(sys.argv[3]))
-    GOTest = AllGoModel(int(sys.argv[2]),sys.argv[1],'AllGO_Original_Struct','Original',foldFile='./src/PairwiseYeastNetwork/AllGOGeneFold_Orignial_1.csv',memMapLoc='../YeastMemMap/YeastCorrDictionary.dat',ontologyDataset='original')
+    GOTest = AllGoModel(int(sys.argv[2]),sys.argv[1],'AllGO_Original_Struct','Original',foldFile='./src/PairwiseYeastNetwork/AllGOGeneFold_Orignial_1.csv',memMapLoc='../YeastMemMap/YeastCorrDictionary.dat',ontologyDataset='original',batch=500)
     GOTest.trainNetwork(150000)
     GOTest.testNetworkAll(runAll=True)
     GOTest.testNetworkAll(runAll=True,validation=False)
