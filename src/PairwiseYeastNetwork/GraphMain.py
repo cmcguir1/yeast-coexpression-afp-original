737426ee6399e27d6527f10810e112eec0b87642
Cole McGuire <cmcguir1@trinity.edu>
2023-03-12 20:51:41 -0500

    Checking new ranking

--- src/PairwiseYeastNetwork/GraphMain.py
+++ src/PairwiseYeastNetwork/GraphMain.py
@@ -28,7 +28,8 @@ def main():
     # GOTest.testNetworkAll(runAll=True,validation=False)
 
     graph = AllGoGraph('./Yeast Resources/Pairwise/Spell/AllGO_Original_Struct_ParaSearch/Original_113x2000x92_Net_fold','113x2000x92','WrongTerm',ontologyDataset='original')
-    graph.feedForward(int(sys.argv[1]),runNegatives=False,offSet=5)
+    #graph.feedForward(int(sys.argv[1]),runNegatives=False,offSet=5)
+    graph.rankGenes()
 
 
 
--- src/TestingCode/TestArea.py
+++ src/TestingCode/TestArea.py
@@ -10,12 +10,19 @@ from Leaf import getLeaves
 def key(leaf):
     return len(leaf[1])
 
+termArray = pd.read_csv('./src/PairwiseYeastNetwork/TermNameDict.csv').to_numpy()
+termDict = {}
+for term in termArray:
+    termDict[term[0]] = term[1]
+
 leaves = getLeaves(10)
 leafTable = []
 for leaf in leaves:
-    leafTable.append([leaf[0],key(leaf)])
+    leafTable.append([leaf[0],termDict[leaf[0]]])
 
-sortedLeaves = sorted(leafTable,key=lambda x: x[1])
-for leaf in sortedLeaves:
-    print(f'{leaf[0]}: {leaf[1]}')
+pd.DataFrame(leafTable,columns=['Term','Description']).to_csv('./GoTermDescriptions.csv',index=False)
+
+# sortedLeaves = sorted(leafTable,key=lambda x: x[1])
+# for leaf in sortedLeaves:
+#     print(f'{leaf[0]}: {leaf[1]}')
 
