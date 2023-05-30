6623c159659e5907d0c7f396d0ccb27821c475c9
Cole McGuire <cmcguir1@trinity.edu>
2023-05-30 12:39:55 -0500

    No One Knows

--- src/PairwiseYeastNetwork/Main.py
+++ src/PairwiseYeastNetwork/Main.py
@@ -34,8 +34,8 @@ def main():
     # GOTest.testNetworkAll(runAll=True,validation=False)
 
     graph = AllGoGraph(f'./Yeast Resources/Pairwise/Spell/BioProcessOnly/BioProcessOnly_113x{sys.argv[2]}x53_Net_fold',f'113x{sys.argv[2]}x53','BioProcOnly',inputVector='x',ontologyDataset='original')
-    graph.feedForward(int(sys.argv[1]),saveAll=True,runBatch=True)
-    # graph.rankGenes()
+    # graph.feedForward(int(sys.argv[1]),saveAll=True,runBatch=True)
+    graph.rankGenes()
     
 
 
--- src/PairwiseYeastNetwork/Main2.py
+++ src/PairwiseYeastNetwork/Main2.py
@@ -34,8 +34,8 @@ def main():
     # GOTest.testNetworkAll(runAll=True,validation=False)
 
     graph = AllGoGraph(f'./Yeast Resources/Pairwise/Spell/bioPIXIE_Data/Original_149x{sys.argv[2]}x53_Net_fold',f'149x{sys.argv[2]}x53','bioPIXIE_Data',inputVector='xlgp',ontologyDataset='original')
-    graph.feedForward(int(sys.argv[1]),saveAll=True,runBatch=True)
-    # graph.rankGenes()
+    # graph.feedForward(int(sys.argv[1]),saveAll=True,runBatch=True)
+    graph.rankGenes()
     
 
 
