0ab09e861a5e66c3ee688af29e390acf2e7008cf
Cole McGuire <cmcguir1@trinity.edu>
2023-05-31 16:37:50 -0500

    Night at the Opera

--- src/PairwiseYeastNetwork/ClusterJobWriter.py
+++ src/PairwiseYeastNetwork/ClusterJobWriter.py
@@ -66,10 +66,10 @@ def writeJobs(JobName,pythonFile,arg1=['0','1','2','3'],arg2=None,arg3=None,arg4
 # writeJobs('bioPIXIE_Data_Graph','Main2.py',arg2=['25000'])
 # writeJobs('bioPIXIE_Data','Main2.py',arg2=['2000','25000'])
 # writeJobs('Overfit_BioProcOnly_Dropout','Main7.py')
-writeJobs('Overfit_bioPIXIE_NoDropout','Main2.py')
-writeJobs('Overfit_bioPIXIE_Dropout','Main.py')
-writeJobs('Overfit_Unreasonable','Main3.py')
-writeJobs('Modern_bioPIXIE','Main4.py')
+writeJobs('DeepNet_bioPIXIE_NoDropout','Main2.py')
+writeJobs('DeepNet_bioPIXIE_Dropout','Main.py')
+# writeJobs('Overfit_Unreasonable','Main3.py')
+# writeJobs('Modern_bioPIXIE','Main4.py')
 
 
 
