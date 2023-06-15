0b726dbc8f96868c1a87127622a1c0c364795146
Cole McGuire <cmcguir1@trinity.edu>
2023-06-15 10:52:20 -0500

    Cowboys from Hell

---
+++ ClusterJobs/CorrelationDict/Main.py
@@ -0,0 +1,31 @@
+from PairwiseYeastData import PairwiseYeastData
+from PairwiseModel import PairwiseModel
+import numpy as np
+import time
+from GeneFolds import GeneFolds
+from YeastGraph import YeastGraph
+import sys
+from ComplexModel import ComplexModel
+import pandas as pd
+import torch
+from AllGoModel import AllGoModel
+from CorrelationDictionary import CorrelationDictionary
+import os
+
+# This import should fix the ssl import verificiation error
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+def main():
+
+   corr = CorrelationDictionary('../YeastMemMap/YeastDict_float16.npy')
+   corr.unifyCorrelations('../YeastMemMap/YeastDict_Redo.dat')
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
--- src/PairwiseYeastNetwork/ClusterJobWriter.py
+++ src/PairwiseYeastNetwork/ClusterJobWriter.py
@@ -71,7 +71,7 @@ def writeJobs(JobName,pythonFile,arg1=['0','1','2','3'],arg2=None,arg3=None,arg4
 # writeJobs('DeepNet_bioPIXIE_NoDropout','Main3.py')
 # writeJobs('DeepNet_bioPIXIE_Dropout','Main4.py')
 # writeJobs('Overfit_Unreasonable','Main3.py')
-writeJobs('Modern_bioPIXIE','Main3.py')
+# writeJobs('Modern_bioPIXIE','Main3.py')
 # writeJobs('Modern_bioPIXIE_Graph','GraphMain.py')
 # writeJobs('Cyclic_lr','Main.py')
 # writeJobs('MitoOrg_ST','SingleTermMain.py',arg1=['GO:0007005'],arg2=['2000'],arg3=['0','1','2','3'])
@@ -80,6 +80,7 @@ writeJobs('Modern_bioPIXIE','Main3.py')
 # writeJobs('ST_ParaSearch','SingleTermMain.py',arg1=['GO:0007005'],arg2=['100','500','1000'],arg3=['0.1','0.01','0.001','0.0001'],arg4=['runAll'])
 # writeJobs('TestClusterSpeed','Main7.py',arg1=['GO:0007005'],arg2=['2000'],arg3=['0.01'],arg4=['0'],arg5=['50','500'])
 # writeJobs('FL_Control','Main6.py',arg2=['FL','CE'])
-writeJobs('LossFunc','Main4.py',arg2=['BCE','CE','FL'])
+# writeJobs('LossFunc','Main4.py',arg2=['BCE','CE','FL'])
+writeJobs('CorrelationDict','Main.py',arg1=[''])
 
 
