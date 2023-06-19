e2765ee0b0406eae8d2439c50eace25f79a96d19
Cole McGuire <cmcguir1@trinity.edu>
2023-06-19 12:43:33 -0500

    Dani California

--- src/PairwiseYeastNetwork/Main.py
+++ src/PairwiseYeastNetwork/Main.py
@@ -18,13 +18,10 @@ ssl._create_default_https_context = ssl._create_unverified_context
 
 def main():
 
-    if sys.argv[2] == 'YeastDict_ReCalc.npy':
-        name = 'ReCalc'
-    else:
-        name = 'Original'
+    
 
-    GOTest = AllGoModel(int(sys.argv[1]),'2000','Test_MemMap_ReCalc2',name,foldFile='./src/PairwiseYeastNetwork/AllGOGeneFold1.csv',ontologyDataset='original',resetNet=True,inputVector='x',cuda=False,batch=50,memMapName=sys.argv[2])
-    GOTest.trainNetwork(40000,track=100)
+    GOTest = AllGoModel(int(sys.argv[1]),'2000','Test_MemMap_ReCalc2','ReCalc_NoReg',foldFile='./src/PairwiseYeastNetwork/AllGOGeneFold1.csv',ontologyDataset='original',resetNet=True,inputVector='x',cuda=False,batch=50,memMapName=sys.argv[2],regularize=False)
+    GOTest.trainNetwork(20000,track=100)
     GOTest.testNetworkAll(runAll=True)
     GOTest.testNetworkAll(runAll=True,validation=False)
 
