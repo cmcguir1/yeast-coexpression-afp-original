661a48509a168fcaed37e6cbdecee4c06383ce42
Cole McGuire <cmcguir1@trinity.edu>
2023-07-15 00:02:14 -0500

    Rocky Raccoon

---
+++ ClusterJobs/RandomizeFeatures/RandomizeFeatures3.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/sh
+
+## Job Created Jul-15-2023
+
+##Place PBS directives here
+#PBS -N RandomizeFeatures
+#PBS -l nodes=1:ppn=36
+#PBS -l walltime=168:00:00
+#PBS -M cmcguir1@trinity.edu
+#PBS -m ae
+
+cd data/SummerResearch2022
+
+/data/hibbslab/anaconda3/bin/python src/PairwiseYeastNetwork/Main4.py 3     > ClusterJobs/RandomizeFeatures/RandomizeFeatures3_output.txt
