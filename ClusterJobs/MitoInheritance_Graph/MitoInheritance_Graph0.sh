ffb0bfd646ca010a056cf7e741b2ec00479aaa8d
Cole McGuire <cmcguir1@trinity.edu>
2024-01-01 00:38:02 -0600

    We Can Get Down

---
+++ ClusterJobs/MitoInheritance_Graph/MitoInheritance_Graph0.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/sh
+
+## Job Created Jan-01-2024
+
+##Place PBS directives here
+#PBS -N MitoInheritance_Graph_0_20
+#PBS -l nodes=1:ppn=36
+#PBS -l walltime=168:00:00
+#PBS -M cmcguir1@trinity.edu
+#PBS -m ae
+
+cd data/SummerResearch2022
+
+/data/hibbslab/anaconda3/bin/python src/PairwiseYeastNetwork/MitoInheritance.py 0 20    > ClusterJobs/MitoInheritance_Graph/MitoInheritance_Graph0_output.txt
