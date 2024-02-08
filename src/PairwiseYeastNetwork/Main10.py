f9157db3def611df4af969d64371b9bd9eca4873
Cole McGuire <cmcguir1@trinity.edu>
2024-02-07 19:45:36 -0600

    The View From The Afternoon

--- src/PairwiseYeastNetwork/Main10.py
+++ src/PairwiseYeastNetwork/Main10.py
@@ -28,6 +28,7 @@ def main():
     graph = AllGoGraph('Yeast Resources/Pairwise/Spell/HardNegatives_Redo/EasyNegatives_x_b_113x100x79_lr0.01_batch50_lfBCE_Net_fold','113x100x79','HardNegatives','EasyNegatives',geneFolds='src/PairwiseYeastNetwork/AllGO_2007_b_1.csv',ontologyDataset='2007',outputVector='b')
     # graph.feedForward(int(sys.argv[1]),calcAgn=False)
     graph.rankGenes(agn=False)
+    
 
 
 
