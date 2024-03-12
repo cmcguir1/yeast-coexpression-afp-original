17ed20dd2e61bffa1178eb38ac72538377a68af1
Cole McGuire <cmcguir1@trinity.edu>
2024-03-11 22:33:37 -0500

    Big Empty

--- src/PairwiseYeastNetwork/Main7.py
+++ src/PairwiseYeastNetwork/Main7.py
@@ -37,7 +37,7 @@ def main():
     # graph.rankGenes(agn=False,singleTerm=True)
 
     graph = AllGoGraph(model.networkLoc[:-5],f'113x{sys.argv[2]}x1',folder,name,geneFolds=foldFile,outputVector='',addTerms=['GO:0007005'],ontologyDataset='2007',evalDataset='2023')
-    # graph.rankGenes(agn=False,singleTerm=True,fileSuffix='_Modern')
+    graph.rankGenes(agn=False,singleTerm=True,fileSuffix='_Modern')
 
 
 
