4a0a5d792456ac121247f93ec5c7a8c291793aed
Cole McGuire <cmcguir1@trinity.edu>
2024-05-17 09:26:50 -0500

    Portrait of a Dead Girl

--- src/PairwiseYeastNetwork/ClusterJobWriter.py
+++ src/PairwiseYeastNetwork/ClusterJobWriter.py
@@ -244,4 +244,4 @@ def writeJobs(JobName,pythonFile,node=False,arg1=['0','1','2','3'],arg2=None,arg
 # writeJobs('MemMap_StressTest_Pairs','TestMain2.py',arg1=['../Cluster_'])
 
 writeJobs('MultiTerm_LinearEval_Replicate_Linear','Main2.py',arg2=['20','200'],arg3=[0],arg1=[0])
-writeJobs('MultiTerm_LinearEval_Replicate_Parallel','Main3.py',arg2=['20','200'],arg3=[1])
+writeJobs('MultiTerm_LinearEval_Replicate_Parallel','Main3.py',arg2=['20','200'],arg3=[1],arg1=[0])
