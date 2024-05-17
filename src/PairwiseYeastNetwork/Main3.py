89269cf2be7f19c74f10b9d025aa6637d0b36ae4
Cole McGuire <cmcguir1@trinity.edu>
2024-05-17 09:34:29 -0500

    Mirror

--- src/PairwiseYeastNetwork/Main3.py
+++ src/PairwiseYeastNetwork/Main3.py
@@ -32,7 +32,7 @@ def main():
     graph = AllGoGraph(model.networkLoc[:-5],f'113x{sys.argv[2]}x79',folder,name,geneFolds=foldFile,outputVector='b',addTerms=[],ontologyDataset='2007',evalDataset='2023')
     # for i in range(4):
     # graph.feedForward(int(sys.argv[1]),calcAgn=True,calcPos=True)
-    graph.rankGenes(agn=True,singleTerm=False)
+    # graph.rankGenes(agn=True,singleTerm=False)
     # graph.rankAllTerms(agn=True)
     graph.debug()
 
