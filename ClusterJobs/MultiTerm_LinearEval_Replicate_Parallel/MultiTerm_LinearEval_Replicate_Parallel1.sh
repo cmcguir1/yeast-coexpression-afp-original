8f9c66f10dda85894a990b7616c7b3348dcdfe4d
Cole McGuire <cmcguir1@trinity.edu>
2024-05-20 10:58:13 -0500

    Aquatic Mouth Dance

--- ClusterJobs/MultiTerm_LinearEval_Replicate_Parallel/MultiTerm_LinearEval_Replicate_Parallel1.sh
+++ ClusterJobs/MultiTerm_LinearEval_Replicate_Parallel/MultiTerm_LinearEval_Replicate_Parallel1.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/sh
 
-## Job Created May-17-2024
+## Job Created May-20-2024
 
 ##Place PBS directives here
-#PBS -N MultiTerm_LinearEval_Replicate_Parallel_1_20_1
+#PBS -N MultiTerm_LinearEval_Replicate_Parallel_0_200_1
 #PBS -l nodes=1:ppn=36
 #PBS -l walltime=168:00:00
 #PBS -M cmcguir1@trinity.edu
@@ -11,4 +11,4 @@
 
 cd data/SummerResearch2022
 
-/data/hibbslab/anaconda3/bin/python src/PairwiseYeastNetwork/Main3.py 1 20 1   > ClusterJobs/MultiTerm_LinearEval_Replicate_Parallel/MultiTerm_LinearEval_Replicate_Parallel1_output.txt
+/data/hibbslab/anaconda3/bin/python src/PairwiseYeastNetwork/Main3.py 0 200 1   > ClusterJobs/MultiTerm_LinearEval_Replicate_Parallel/MultiTerm_LinearEval_Replicate_Parallel1_output.txt
