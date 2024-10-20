d69a7c77bbe536efe4fe883b1e3cfcbd6058b341
Cole McGuire <cmcguir1@trinity.edu>
2024-10-20 18:06:05 -0500

    Spanish Bombs

---
+++ ClusterJobs/OverfittingTest_Graph/OverfittingTest_Graph1.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/sh
+
+## Job Created Oct-20-2024
+
+##Place PBS directives here
+#PBS -N OverfittingTest_Graph_1_20_300000
+#PBS -l nodes=1:ppn=36
+#PBS -l walltime=168:00:00
+#PBS -M cmcguir1@trinity.edu
+#PBS -m ae
+
+cd data/SummerResearch2022
+
+/data/hibbslab/anaconda3/bin/python src/PairwiseYeastNetwork/Main4.py 1 20 300000   > ClusterJobs/OverfittingTest_Graph/OverfittingTest_Graph1_output.txt
