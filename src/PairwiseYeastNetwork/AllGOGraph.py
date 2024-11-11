7e80de7141d89619d7710923913816cb4dd501fe
Cole McGuire <cmcguir1@trinity.edu>
2024-11-10 23:57:37 -0600

    Aladdin Sane

--- src/PairwiseYeastNetwork/AllGOGraph.py
+++ src/PairwiseYeastNetwork/AllGOGraph.py
@@ -483,7 +483,7 @@ class AllGoGraph(AllGoModel):
         summary = []
         l = self.evalLeaves if modern else self.leaves
         for term, _ in l:
-            if term in self.indexDict:
+            if term in self.GOTermDict:
                 auc, avgPrec = self.rankGenes(term=term,agn=agn,fileSuffix=fileSuffix,modern=modern)
                 summary.append([term,auc,avgPrec])
         pd.DataFrame(summary,columns=['GO Term','AUC','Average Precision']).to_csv(f'{self.path}/{"" if self.modelName == "" else f"_{self.modelName}"}{self.struct}GOTermDistribution{fileSuffix}.csv',index=False)
